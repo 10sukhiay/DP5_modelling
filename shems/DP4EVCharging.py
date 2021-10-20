8b44b81d81ea2768e01e4aec38c7fb26bfbff83b
ajh2 <ajhonnywill@gmail.com>
2021-10-20 11:46:41 +0100

    Started porting Matlab DP4 charger to Python

---
+++ shems/DP4EVCharging.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import os
+import datetime
+
+arrival_time = datetime.datetime(2019, 2, 25, 19, 15, 00)
+departure_time = datetime.datetime(2019, 2, 27, 19, 15, 00)
+agile_extract = pd.read_excel(os.getcwd()[:-5] + 'Inputs\AgileExtract.xlsx')
+
+test = 'end'
