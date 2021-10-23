cdbde6b7319465a36384fa93e682dc8f75b0b0d2
ajh2 <ajhonnywill@gmail.com>
2021-10-23 14:39:09 +0100

    Removed chained indexing from vrg and added variables for virtual cost calculations

--- shems/DP4EVCharging.py
+++ shems/DP4EVCharging.py
@@ -5,12 +5,13 @@ import datetime
 import matplotlib.pyplot as plt
 
 
-def vrg(charging_intervals, charge_schedule, connections_available):
-    while charge_schedule['Charge_Rate'].sum() < charging_intervals:
-        time_to_charge = connections_available.index.min()
-        charge_schedule.loc[time_to_charge, 'Charge_Rate'] = 1
-        connections_available.drop(time_to_charge, inplace=True)
-    return charge_schedule, connections_available
+def vrg(charge_duration, charge_schedule):
+    # print(charge_schedule.iloc[1,0])
+    # print(charge_schedule.loc[: pd.to_datetime('2019-02-26 20:00:00'), 'Charge_Rate'])
+    charge_schedule.loc[: charge_schedule.index.min() + charge_duration, 'Charge_Rate'] = 1
+    # charge_schedule.iloc[0 : vrg_charging_intervals, 1] = 1
+    # charge_schedule.first(vrg_charge_duration)['Charge_Rate'] = 1
+    return charge_schedule
 
 
 def v1g(charging_intervals, charge_schedule, connections_available):
@@ -22,33 +23,38 @@ def v1g(charging_intervals, charge_schedule, connections_available):
     return charge_schedule, connections_available
 
 
+# def update_SoC(charge_schedule, time_resolution, charge_rate, at_time):
+
+charge_rate = 7.4  # kW
+max_battery_cycles = 1500  # for TM3
+charger_efficiency = 1  # for charger
+
 arrival_time = pd.to_datetime('2019-02-25 19:15:00')
-departure_time = pd.to_datetime('2019-02-26 12:00:00')
+departure_time = pd.to_datetime('2019-02-26 1:00:00')
 time_resolution = '15 min'
 
 vrg_charge_duration = pd.Timedelta('105 min')
 v1g_charge_duration = pd.Timedelta('310 min')
-vrg_charging_intervals = vrg_charge_duration/time_resolution
-v1g_charging_intervals = v1g_charge_duration/time_resolution
+vrg_charging_intervals = vrg_charge_duration / time_resolution
+v1g_charging_intervals = v1g_charge_duration / time_resolution
 
 agile_extract = pd.read_excel(os.getcwd()[:-5] + 'Inputs\AgileExtract.xls', parse_dates=[0], index_col=0)
 connection_extract = agile_extract[arrival_time : departure_time].resample(time_resolution).pad().iloc[:-1, :]
+
+# print(connection_extract[connection_extract.index.min() : departure_time - pd.Timedelta('40 min')].sum())
 
 initial_connections_available = connection_extract.copy()
 zeros_charge_schedule = connection_extract
 zeros_charge_schedule['Charge_Rate'] = 0
 
-vrg_charge_schedule, vrg_connections_available = vrg(vrg_charging_intervals,
-                                                     zeros_charge_schedule,
-                                                     initial_connections_available)
+vrg_charge_schedule = vrg(vrg_charge_duration,
+                          zeros_charge_schedule)
 
-v1g_charge_schedule, v1g_connections_available = v1g(v1g_charging_intervals,
-                                                     vrg_charge_schedule.copy(),
-                                                     vrg_connections_available.copy())
+# v1g_charge_schedule, v1g_connections_available = v1g(vrg_charging_intervals,
+#                                                      vrg_charge_schedule.copy(),
+#                                                      vrg_connections_available.copy())
 
 print(vrg_charge_schedule)
-print(vrg_connections_available)
-print(v1g_charge_schedule)
-print(v1g_connections_available)
-
-test
+# print(vrg_connections_available)
+# print(v1g_charge_schedule)
+# print(v1g_connections_available)
