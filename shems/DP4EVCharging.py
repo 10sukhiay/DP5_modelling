f059404be0fedd43b6d024c7c920ec3211521c3c
ajh2 <ajhonnywill@gmail.com>
2021-10-24 18:53:54 +0100

    Recalculated SoC_resolution from new variable kWh_resolution

--- shems/DP4EVCharging.py
+++ shems/DP4EVCharging.py
@@ -12,22 +12,18 @@ def vrg(charge_duration, charge_schedule):
 
 def v1g(charge_duration, charge_schedule):
     v1g_charge_schedule = charge_schedule[charge_schedule['Charger_Instruction'] == 0].copy()
-    # print(v1g_charge_schedule)
     v1g_charge_schedule.loc[:, 'Charger_Instruction'] = 2
-    # print(v1g_charge_schedule)
     charge_schedule.loc[charge_schedule['Charger_Instruction'] == 0, :] = v1g_charge_schedule
-    # print(charge_schedule)
     return charge_schedule
 
 
 def calculate_SoC(charge_schedule, SoC_resolution):
-    # print(charge_schedule)
-    # print(charge_schedule['Charger_Instruction'])
-    # print(charge_schedule['Charger_Instruction'].copy().cumsum()*SoC_resolution)
     cumsumSoC = charge_schedule['Charger_Instruction'].copy().cumsum() * SoC_resolution + charge_schedule.loc[charge_schedule.index.min(), 'SoC']
     charge_schedule.iloc[1:, charge_schedule.columns.get_indexer(['SoC'])] = cumsumSoC[:-1]
-    # print(charge_schedule)
     return charge_schedule
+
+
+# def virtual_cost(charge_schedule, timeSoC_resolution)
 
 
 # def v1g(charging_intervals, charge_schedule):
@@ -50,12 +46,13 @@ plug_in_SoC = 0.1
 arrival_time = pd.to_datetime('2019-02-25 19:15:00')
 departure_time = pd.to_datetime('2019-02-26 1:00:00')
 time_resolution = pd.Timedelta('15 min')
+kWh_resolution = charge_rate * time_resolution / pd.Timedelta('60 min')
+SoC_resolution = kWh_resolution / battery_capacity
 
 vrg_charge_duration = pd.Timedelta('105 min')
 v1g_charge_duration = pd.Timedelta('310 min')
 # vrg_charging_intervals = vrg_charge_duration / time_resolution
 v1g_charging_intervals = v1g_charge_duration / time_resolution
-SoC_resolution = time_resolution / pd.Timedelta(str(battery_capacity / charge_rate) + 'h')
 
 agile_extract = pd.read_excel(os.getcwd()[:-5] + 'Inputs\AgileExtract.xls', parse_dates=[0], index_col=0)
 connection_extract = agile_extract[arrival_time: departure_time].resample(time_resolution).pad().iloc[:-1, :]
