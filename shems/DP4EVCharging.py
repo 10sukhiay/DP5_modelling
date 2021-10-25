46cdbdcf270184824e188e2c27a46c7145b1f04b
ajh2 <ajhonnywill@gmail.com>
2021-10-25 01:28:27 +0100

    Massive improvements over DP4: vrg and v1g are vectorised (improving compute time dramatically), and battery health metrics are now considered in an efficient virtual cost metric. The battery health modifies the cycle cost by factors that consider the state of charge and the length of time the state of charge is held for, e.g., it wants to maintain charge around 15% for the full connection period

--- shems/DP4EVCharging.py
+++ shems/DP4EVCharging.py
@@ -5,26 +5,41 @@ import datetime
 import matplotlib.pyplot as plt
 
 
-def vrg(charge_duration, charge_schedule):
-    charge_schedule.loc[: charge_schedule.index.min() + charge_duration, 'Charger_Instruction'] = 1
+def vrg(charge_schedule):
+    charge_schedule.loc[: charge_schedule.index.min() + vrg_charge_duration, 'Charger_Instruction'] = 1
     return charge_schedule
 
 
-def v1g(charge_duration, charge_schedule):
-    v1g_charge_schedule = charge_schedule[charge_schedule['Charger_Instruction'] == 0].copy()
-    v1g_charge_schedule.loc[:, 'Charger_Instruction'] = 2
-    charge_schedule.loc[charge_schedule['Charger_Instruction'] == 0, :] = v1g_charge_schedule
+def v1g(charge_schedule):
+    while charge_schedule['Charger_Instruction'].sum() * time_resolution <= v1g_charge_duration + vrg_charge_duration:
+        calculate_soc(charge_schedule)
+        virtual_cost(charge_schedule)
+        working_charge_schedule = charge_schedule[charge_schedule['Charger_Instruction'] == 0]
+        charge_schedule.loc[working_charge_schedule['Virtual_Cost'].idxmin(), 'Charger_Instruction'] = 1
+        test = 1
     return charge_schedule
 
 
 def calculate_soc(charge_schedule):
-    cumsum_soc = charge_schedule['Charger_Instruction'].copy().cumsum() * SoC_resolution +\
-                charge_schedule.loc[charge_schedule.index.min(), 'SoC']
+    cumsum_soc = charge_schedule['Charger_Instruction'].copy().cumsum() * SoC_resolution + \
+                 charge_schedule.loc[charge_schedule.index.min(), 'SoC']
     charge_schedule.iloc[1:, charge_schedule.columns.get_indexer(['SoC'])] = cumsum_soc[:-1]
     return charge_schedule
 
 
-# def virtual_cost(charge_schedule, charger_efficiency, kWh_resolution, )
+def virtual_cost(charge_schedule, charger_type='V1G'):
+    if charger_type == 'V1G':
+        charge_schedule['Soc_From_15'] = calculate_soc(charge_schedule)['SoC'] / 3 + 0.30
+        soc_from_15 = calculate_soc(charge_schedule)['SoC'] / 3 + 0.30
+        charge_held_fraction = (departure_time - charge_schedule.index.to_series()) / \
+                               (departure_time - arrival_time)
+        charge_schedule['Battery_Life_Factor'] = cycle_cost_fraction / (1 - soc_from_15 * charge_held_fraction)
+        beta = 0
+
+    charge_schedule['Virtual_Cost'] = charge_schedule['Price'] * kWh_resolution / charger_efficiency + \
+                                      charge_schedule['Battery_Life_Factor'] + beta
+
+    return charge_schedule
 
 
 # def v1g(charging_intervals, charge_schedule):
@@ -39,21 +54,21 @@ def calculate_soc(charge_schedule):
 # def update_SoC(charge_schedule, time_resolution, charge_rate, at_time):
 
 charge_rate = 7.4  # kW
-max_battery_cycles = 1500  # for TM3
+max_battery_cycles = 1500 * 1.625  # for TM3
 battery_capacity = 54  # kWh
 charger_efficiency = 1  # for charger
 plug_in_SoC = 0.1
 battery_cost_per_kWh = 137e2
 
 arrival_time = pd.to_datetime('2019-02-25 19:15:00')
-departure_time = pd.to_datetime('2019-02-26 1:00:00')
+departure_time = pd.to_datetime('2019-02-26 19:00:00')
 time_resolution = pd.Timedelta('15 min')
-# kWh_resolution = charge_rate * time_resolution / pd.Timedelta('60 min')
-cycle_cost_fraction = battery_cost_per_kWh * charge_rate * time_resolution / pd.Timedelta('60 min') / max_battery_cycles
+kWh_resolution = charge_rate * time_resolution / pd.Timedelta('60 min')
+cycle_cost_fraction = battery_cost_per_kWh * kWh_resolution / max_battery_cycles
 SoC_resolution = cycle_cost_fraction * max_battery_cycles / battery_capacity / battery_cost_per_kWh
 
-vrg_charge_duration = pd.Timedelta('105 min')
-v1g_charge_duration = pd.Timedelta('310 min')
+vrg_charge_duration = pd.Timedelta('40 min')
+v1g_charge_duration = pd.Timedelta('300 min')
 # vrg_charging_intervals = vrg_charge_duration / time_resolution
 v1g_charging_intervals = v1g_charge_duration / time_resolution
 
@@ -67,16 +82,25 @@ zeros_charge_schedule = connection_extract
 zeros_charge_schedule['Charger_Instruction'] = 0
 zeros_charge_schedule.loc[zeros_charge_schedule.index.min(), 'SoC'] = plug_in_SoC
 
-vrg_charge_schedule = vrg(vrg_charge_duration,
-                          zeros_charge_schedule)
-#
-# v1g_charge_schedule = v1g(vrg_charge_schedule,
-#                           vrg_charge_schedule.copy())
+vrg_charge_schedule = vrg(zeros_charge_schedule)
 
-test = calculate_soc(vrg_charge_schedule)
+v1g_charge_schedule = v1g(vrg_charge_schedule)
 
-print(test)
-# print(vrg_charge_schedule)
-# print(vrg_connections_available)
-# print(v1g_charge_schedule)
-# print(v1g_connections_available)
+# test = calculate_soc(vrg_charge_schedule)
+
+# test = virtual_cost(vrg_charge_schedule)
+
+v1g_total_cost = (v1g_charge_schedule['Charger_Instruction'] * v1g_charge_schedule['Virtual_Cost']).sum()
+v1g_charge_schedule['Wholesale_Cost'] = v1g_charge_schedule['Price'] * kWh_resolution
+v1g_charge_schedule['Cost_Ratio'] = v1g_charge_schedule['Wholesale_Cost'] / v1g_charge_schedule['Virtual_Cost']
+
+plt.subplot(411)
+plt.plot(v1g_charge_schedule['Price'])
+plt.subplot(412)
+plt.plot(v1g_charge_schedule['Battery_Life_Factor'])
+plt.subplot(413)
+plt.plot(v1g_charge_schedule['Virtual_Cost'])
+plt.subplot(414)
+plt.plot(v1g_charge_schedule['SoC'])
+plt.show()
+
