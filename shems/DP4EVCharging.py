953579478a38816a53660d3918f19db45927fd5a
ajh2 <ajhonnywill@gmail.com>
2021-10-28 15:05:45 +0100

    V2G charge logic implemented

--- shems/DP4EVCharging.py
+++ shems/DP4EVCharging.py
@@ -4,45 +4,49 @@ import matplotlib.pyplot as plt
 
 
 def vrg(charge_schedule):
-    charge_schedule.loc[: charge_schedule.index.min() + vrg_charge_duration, 'Charger_Instruction'] = 1
+    charge_schedule.loc[: charge_schedule.index.min() + vrg_charge_duration, 'Charge_In_Interval'] = 1
     return charge_schedule
 
 
 def v1g(charge_schedule):
-    while charge_schedule['Charger_Instruction'].sum() * time_resolution <= v1g_charge_duration + vrg_charge_duration:
+    while charge_schedule['Charge_In_Interval'].sum() * time_resolution <= v1g_charge_duration + vrg_charge_duration:
         calculate_soc(charge_schedule)
         virtual_cost(charge_schedule, 'v1g')
-        working_charge_schedule = charge_schedule[charge_schedule['Charger_Instruction'] == 0]
-        charge_schedule.loc[working_charge_schedule['Virtual_Cost'].idxmin(), 'Charger_Instruction'] = 1
+        working_charge_schedule = charge_schedule[charge_schedule['Charge_In_Interval'] == 0].iloc[:-1, :]
+        charge_schedule.loc[working_charge_schedule['Virtual_Cost'].idxmin(), 'Charge_In_Interval'] = 1
     return charge_schedule
 
 
 def v2g(charge_schedule):
-    charge_schedule['Checked'] = charge_schedule['Charger_Instruction'].copy()
-    v2g_total_cost = (charge_schedule['Charger_Instruction'] * v1g_charge_schedule['Virtual_Cost']).sum()
-    while charge_schedule['Checked'].sum() < charge_schedule.shape[0]:
+    charge_schedule['Checked'] = charge_schedule['Charge_In_Interval'].copy()
+    v2g_total_cost = (charge_schedule['Charge_In_Interval'] * v1g_charge_schedule['Virtual_Cost']).sum()
+    while charge_schedule['Checked'].sum() < charge_schedule.shape[0] - 1:
         calculate_soc(charge_schedule)
 
-        working_charge_schedule = charge_schedule[charge_schedule['Checked'] == 0].copy()
+        working_charge_schedule = charge_schedule[charge_schedule['Checked'] == 0].iloc[:-1, :]
         discharge_time = working_charge_schedule['Price'].idxmax()
         discharge_time_mask = charge_schedule.index.to_series().isin(working_charge_schedule.index.to_series().values)
         charge_schedule.loc[discharge_time_mask, 'Discharge_Time'] = discharge_time
         virtual_cost(charge_schedule, 'v2g')
         charge_schedule.loc[discharge_time, 'Checked'] = 1
-        working_charge_schedule = charge_schedule[charge_schedule['Checked'] == 0].copy()
-        # test = working_charge_schedule['Virtual_Cost'].min()
+        working_charge_schedule = charge_schedule[charge_schedule['Checked'] == 0].iloc[:-1, :]
 
         if working_charge_schedule['Virtual_Net'].min() < 0:
-            charge_schedule.loc[working_charge_schedule['Virtual_Net'].idxmin(), 'Charger_Instruction'] = 1
+            charge_schedule.loc[working_charge_schedule['Virtual_Net'].idxmin(), 'Charge_In_Interval'] = 1
             charge_schedule.loc[working_charge_schedule['Virtual_Net'].idxmin(), 'Checked'] = 1
-            charge_schedule.loc[working_charge_schedule['Price'].idxmax(), 'Charger_Instruction'] = -1
-            # v2g_total_cost += working_charge_schedule['Virtual_Cost'].min()
+            charge_schedule.loc[discharge_time, 'Charge_In_Interval'] = -1
+            v2g_total_cost += working_charge_schedule['Virtual_Cost'].min()
+            calculate_soc(charge_schedule)
+            # if charge_schedule['SoC']
+
+    # while working_charge_schedule['Virtual_Net'].min() < 0:
+
 
     return charge_schedule, v2g_total_cost
 
 
 def calculate_soc(charge_schedule):
-    cumsum_soc = charge_schedule['Charger_Instruction'].copy().cumsum() * SoC_resolution + \
+    cumsum_soc = charge_schedule['Charge_In_Interval'].copy().cumsum() * SoC_resolution + \
                  charge_schedule.loc[charge_schedule.index.min(), 'SoC']
     charge_schedule.iloc[1:, charge_schedule.columns.get_indexer(['SoC'])] = cumsum_soc[:-1]
     return charge_schedule
@@ -76,12 +80,12 @@ def virtual_cost(charge_schedule, charger_type='v1g'):
         # test3 = test2.loc[test2.index.min(), 'Price']
         test4 = charge_schedule.loc[charge_schedule['Discharge_Time'].copy(), 'Price'].values
         test5 = test4 * kWh_resolution * charger_efficiency
-        charge_schedule['Virtual_Revenue'] = maker_taker_cost - test5
-        charge_schedule.loc[charge_schedule.index.max(), 'Virtual_Revenue'] = 0
+        charge_schedule['Virtual_Revenue'] = test5 - maker_taker_cost
+        # charge_schedule.loc[charge_schedule.index.max(), 'Virtual_Revenue'] = 0
 
     charge_schedule['Virtual_Cost'] = charge_schedule['Price'] * kWh_resolution / charger_efficiency + \
                                       battery_ageing_cost
-    charge_schedule['Virtual_Net'] = charge_schedule['Virtual_Cost'] + charge_schedule['Virtual_Revenue']
+    charge_schedule['Virtual_Net'] = charge_schedule['Virtual_Cost'] - charge_schedule['Virtual_Revenue']
 
     return charge_schedule
 
@@ -107,13 +111,15 @@ v1g_charge_duration = pd.Timedelta('50 min')
 v1g_charging_intervals = v1g_charge_duration / time_resolution
 
 agile_extract = pd.read_excel(os.getcwd()[:-5] + 'Inputs\AgileExtract.xls', parse_dates=[0], index_col=0)
-connection_extract = agile_extract[arrival_time: departure_time].resample(time_resolution).pad().iloc[:-1, :]
+connection_extract = agile_extract[arrival_time: departure_time].resample(time_resolution).pad()  # .iloc[:-1, :]
+connection_extract.loc[connection_extract.index.max(), 'Price'] = maker_taker_cost / kWh_resolution / charger_efficiency  # offset v1g and vrg revenue to 0 - this is kind of a hack
+
 
 # print(connection_extract[connection_extract.index.min() : departure_time - pd.Timedelta('40 min')].sum())
 
 # initial_connections_available = connection_extract.copy()
 zeros_charge_schedule = connection_extract
-zeros_charge_schedule['Charger_Instruction'] = 0
+zeros_charge_schedule['Charge_In_Interval'] = 0
 zeros_charge_schedule.loc[zeros_charge_schedule.index.min(), 'SoC'] = plug_in_SoC
 
 vrg_charge_schedule = vrg(zeros_charge_schedule)
@@ -130,11 +136,11 @@ plt.show()
 
 # test = virtual_cost(vrg_charge_schedule)
 
-v1g_total_cost = (v1g_charge_schedule['Charger_Instruction'] * v1g_charge_schedule['Virtual_Cost']).sum()
+v1g_total_cost = (v1g_charge_schedule['Charge_In_Interval'] * v1g_charge_schedule['Virtual_Cost']).sum()
 # v1g_charge_schedule['Wholesale_Cost'] = v1g_charge_schedule['Price'] * kWh_resolution
 # v1g_charge_schedule['Cost_Ratio'] = v1g_charge_schedule['Wholesale_Cost'] / v1g_charge_schedule['Virtual_Cost']
 v2g_total_cost_check = (
-        v2g_charge_schedule['Charger_Instruction'] * v2g_charge_schedule['Price'] * kWh_resolution).sum()
+        v2g_charge_schedule['Charge_In_Interval'] * v2g_charge_schedule['Price'] * kWh_resolution).sum()
 
 print(v1g_total_cost)
 print(v2g_total_cost)
