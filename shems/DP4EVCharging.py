8040af8dad08a4b3eaafe3a0aaf08b917eb65631
ajh2 <ajhonnywill@gmail.com>
2022-02-06 22:12:29 +0000

    Fixed a bug with integrated heating indentation missing an update

--- shems/DP4EVCharging.py
+++ shems/DP4EVCharging.py
@@ -117,8 +117,8 @@ def calculate_running_cost(charge_schedule):
     home_by_grid = (charge_schedule['Charge_In_Interval'].values == 0) + battery_charge
 
     test = home_by_grid * charge_schedule['Home_Power'] * kW_to_kWh
-    # change_in_cost = battery_charge * charge_schedule['Virtual_Cost'] - battery_discharge * charge_schedule['Virtual_Revenue'] + home_by_grid * charge_schedule['Home_Power'] * kW_to_kWh
-    change_in_cost = home_by_grid * charge_schedule['Home_Power'] * kW_to_kWh * charge_schedule['Price']
+    change_in_cost = battery_charge * charge_schedule['Virtual_Cost'] - battery_discharge * charge_schedule['Virtual_Revenue'] + home_by_grid * charge_schedule['Home_Power'] * kW_to_kWh
+    # change_in_cost = home_by_grid * charge_schedule['Home_Power'] * kW_to_kWh * charge_schedule['Price']
     cumsum_cost = change_in_cost.cumsum()  # cumulatively sum cost and revenue from each interval
     test2 = test.cumsum()
     charge_schedule['Running_Cost'] = 0  # initialise column
@@ -189,7 +189,8 @@ def virtual_cost(charge_schedule, charger_type):
         charge_held_fraction = (charge_schedule['Discharge_Time'] - charge_schedule.index.to_series()) / (departure_time - arrival_time)
         cycle_cost_fraction = battery_cost_per_kWh * kWh_resolution * discharge_power_frac / max_battery_cycles  # cost of battery wear due to charging and discharging
         battery_ageing_cost = cycle_cost_fraction / (1 - soc_from_15 * charge_held_fraction * lifetime_ageing_factor)
-        charge_schedule['Virtual_Cost'] = charge_schedule['Price'] * kWh_resolution * discharge_power_frac / charger_efficiency + battery_ageing_cost
+        # charge_schedule['Virtual_Cost'] = charge_schedule['Price'] * kWh_resolution * discharge_power_frac / charger_efficiency + battery_ageing_cost
+        charge_schedule['Virtual_Cost'] = charge_schedule['Price'] * kWh_resolution / charger_efficiency + battery_ageing_cost
 
     elif charger_type == 'v2h':
         discharge_price = charge_schedule.loc[charge_schedule['Discharge_Time'], 'Price'].values * charger_efficiency
@@ -246,7 +247,7 @@ def plot_vr12g(charge_schedule_vrg, charge_schedule_v1g, charge_schedule_v2g, ch
     plt.show()
 
 
-def initialise_charge_schedule(appliance_forecast=True, gas=True):
+def initialise_charge_schedule(appliance_forecast=True, gas=False):
     agile_extract = pd.read_excel(os.getcwd()[:-5] + tariff_data, parse_dates=[0], index_col=0).resample(time_resolution).pad()
     connection_extract = agile_extract[arrival_time: departure_time].copy()  # .iloc[:-1, :]
     connection_extract_mean_price = connection_extract['Price'].mean()
@@ -265,12 +266,12 @@ def initialise_charge_schedule(appliance_forecast=True, gas=True):
             total_gas_cost = gas_cost.cumsum()[-1]
         else:
             connection_extract['Home_Power'] = connection_extract['Appliance_Power'] - connection_extract['Solar_Power'] + connection_extract['Heating_Power']
-            gas_cost = 0
+            total_gas_cost = 0
     else:
         connection_extract['Appliance_Power'] = connection_extract['Price'] * 0  # BODGE
         connection_extract['Solar_Power'] = connection_extract['Price'] * 0  # BODGE
 
-    return connection_extract, gas_cost
+    return connection_extract, total_gas_cost
 
 
 """Inputs from researched data. IDEA: make readable as .txt batch files"""
@@ -286,8 +287,8 @@ lifetime_ageing_factor = 1  # 1
 max_battery_cycles = 1500 * 1.625  # * (1 + 0.625 * lifetime_ageing_factor)  # for TM3, factored to account for factory rating including lifetime degradation 65/40
 price_volatility_factor = 1  # 1
 tariff_data = 'Inputs\AgileExtract.xls'
-arrival_time = pd.to_datetime('2019-02-17 00:00:00')  # '2019-02-25 19:00:00' Bugged: '2019-07-23 19:00:00'
-departure_time = pd.to_datetime('2019-02-27 00:00:00')  # '2019-02-27 07:00:00' Bugged: '2019-07-26 07:00:00'
+arrival_time = pd.to_datetime('2019-02-25 19:00:00')  # '2019-02-25 19:00:00' Bugged: '2019-07-23 19:00:00'
+departure_time = pd.to_datetime('2019-02-27 07:00:00')  # '2019-02-27 07:00:00' Bugged: '2019-07-26 07:00:00'
 time_resolution = pd.Timedelta('15 min')
 vrg_charge_duration = pd.Timedelta('1.6 h')  # 1.6 TO be provided by Yaz's algo to calculate energy from distance
 v1g_charge_duration = pd.Timedelta('2 h')  # 2 TO be provided by Yaz's algo to calculate energy from distance
@@ -304,12 +305,13 @@ app_demand_series_frac = app_demand_series.resample(time_resolution).mean()
 # plt.show()
 
 """Main body of code"""
-zeros_charge_schedule, total_gas_cost = initialise_charge_schedule()
+zeros_charge_schedule, gas_cost = initialise_charge_schedule()
 vrg_charge_schedule = vrg(zeros_charge_schedule, battery_mode)
 vrg_charge_schedule_max = virtual_cost(calculate_soc(vrg_max(zeros_charge_schedule.copy(), battery_mode)), 'v1g')
 v1g_charge_schedule = v1g(vrg_charge_schedule.copy(), battery_mode)
 v2g_charge_schedule = v2g(v1g_charge_schedule.copy(), battery_mode)
-v2h_charge_schedule = v2h(v2g_charge_schedule.copy())
+# v2h_charge_schedule = v2h(v2g_charge_schedule.copy())
+v2h_charge_schedule = v2h(v1g_charge_schedule.copy())
 
 calculate_running_cost(vrg_charge_schedule_max)
 calculate_running_cost(v1g_charge_schedule)
--- shems/IntergratedHeating.py
+++ shems/IntergratedHeating.py
@@ -23,11 +23,11 @@ def main(arrival_time, departure_time, time_resolution):
     OutsideTempData = pd.read_excel(os.getcwd()[:-5] + 'Inputs\HomeGen\Temp1.xls', parse_dates=[0], index_col=0).resample(time_resolution).interpolate()
     MaskedOutsideTemp = OutsideTempData[arrival_time: departure_time].copy()
 
-    Outside_Temp = MaskedOutsideTemp.iloc[Tempno,0];
-    Inside_Temp = 18;
-    Desired_Temp = 24;
-    Inside_Temp_Change = Desired_Temp - Inside_Temp;
-    Outside_Temp_Change = Inside_Temp - Outside_Temp;
+    Outside_Temp = MaskedOutsideTemp.iloc[Tempno, 0]
+    Inside_Temp = 18
+    Desired_Temp = 24
+    Inside_Temp_Change = Desired_Temp - Inside_Temp
+    Outside_Temp_Change = Inside_Temp - Outside_Temp
     Temp_Data = []
     Time = 0
     Time_Data = []
@@ -36,38 +36,37 @@ def main(arrival_time, departure_time, time_resolution):
 
     Power_df = pd.DataFrame(columns=['Power'])
 
-    
-    Total_Window_Area = Window_Area * No_Windows;
-    Total_Door_Area = Door_Area * No_Doors;
+    Total_Window_Area = Window_Area * No_Windows
+    Total_Door_Area = Door_Area * No_Doors
     Wall_Area = (Wall_Height * Wall_Length) - Total_Window_Area/4 - Total_Door_Area/4;
-    Room_Volume = Wall_Height * Wall_Length * Wall_Length;
-    
+    Room_Volume = Wall_Height * Wall_Length * Wall_Length
+
     ## Energy Needed to heat the room ##
-    Density_Air = 1.225;
-    SHC_Air = 1000;
+    Density_Air = 1.225
+    SHC_Air = 1000
         
     ## Loss through walls,roof and floor ##
-    WallU = 0.3;
-    FloorU = 0.22;
-    RoofU = 0.16;
-    WindowU = 1.8;
-    Window_Area = 1.2 * 2;
-    No_Windows =  4
-    DoorU = 3;
+    WallU = 0.3
+    FloorU = 0.22
+    RoofU = 0.16
+    WindowU = 1.8
+    Window_Area = 1.2 * 2
+    No_Windows = 4
+    DoorU = 3
     Door_Area = 2
     No_Doors = 2
     
     
     ## Room heating ##
-    Heating = 1000;
+    Heating = 1000
 
     total_rows = len(MaskedOutsideTemp)*time_res
     
-    while Time < total_rows :
+    while Time < total_rows:
         if Inside_Temp < Desired_Temp:
             
-            Outside_Temp = MaskedOutsideTemp.iloc[Tempno,0];
-            Outside_Temp_Change = Inside_Temp - Outside_Temp;
+            Outside_Temp = MaskedOutsideTemp.iloc[Tempno, 0]
+            Outside_Temp_Change = Inside_Temp - Outside_Temp
             
             Wall_Loss = WallU * (Wall_Area * 4) * Outside_Temp_Change
             Floor_Loss = FloorU * (Wall_Length * 2) * Outside_Temp_Change
@@ -77,39 +76,40 @@ def main(arrival_time, departure_time, time_resolution):
             Energy_Loss = (Wall_Loss + Floor_Loss + Roof_Loss + Window_Loss + Door_Loss) * 1.1
         
         
-            Per_Second_change = Heating - Energy_Loss;
-            Per_min_Energy = Per_Second_change * 60 * time_res ;
+            Per_Second_change = Heating - Energy_Loss
+            Per_min_Energy = Per_Second_change * 60 * time_res
             
             Inside_Temp_Heating = (Per_min_Energy) / (Density_Air * Room_Volume) / SHC_Air
-            Inside_Temp = Inside_Temp + Inside_Temp_Heating;
+            Inside_Temp = Inside_Temp + Inside_Temp_Heating
         
             Time = Time + time_res
             Time_Data.append(Time)
-            Tempno = Tempno + 1;
+            Tempno = Tempno + 1
             Power_df = Power_df.append({'Power': Heating / 1000}, ignore_index=True)
         
         else:
             Heating = Energy_Loss
-        Outside_Temp = MaskedOutsideTemp.iloc[Tempno,0];
-        Outside_Temp_Change = Inside_Temp - Outside_Temp;
 
-        Wall_Loss = WallU * (Wall_Area * 4) * Outside_Temp_Change
-        Floor_Loss = FloorU * (Wall_Length * 2) * Outside_Temp_Change
-        Roof_Loss = RoofU * (Wall_Length * 2) * Outside_Temp_Change
-        Window_Loss = WindowU * (Window_Area * No_Windows) * Outside_Temp_Change
-        Door_Loss = DoorU * (Door_Area * No_Doors) * Outside_Temp_Change
-        Energy_Loss = (Wall_Loss + Floor_Loss + Roof_Loss + Window_Loss + Door_Loss) * 1.1
+            Outside_Temp = MaskedOutsideTemp.iloc[Tempno, 0]
+            Outside_Temp_Change = Inside_Temp - Outside_Temp
 
-        Per_Second_change = Heating - Energy_Loss;
-        Per_min_Energy = Per_Second_change * 60 * time_res;
+            Wall_Loss = WallU * (Wall_Area * 4) * Outside_Temp_Change
+            Floor_Loss = FloorU * (Wall_Length * 2) * Outside_Temp_Change
+            Roof_Loss = RoofU * (Wall_Length * 2) * Outside_Temp_Change
+            Window_Loss = WindowU * (Window_Area * No_Windows) * Outside_Temp_Change
+            Door_Loss = DoorU * (Door_Area * No_Doors) * Outside_Temp_Change
+            Energy_Loss = (Wall_Loss + Floor_Loss + Roof_Loss + Window_Loss + Door_Loss) * 1.1
 
-        Inside_Temp_Heating = (Per_min_Energy) / (Density_Air * Room_Volume) / SHC_Air
-        Inside_Temp = Inside_Temp + Inside_Temp_Heating;
+            Per_Second_change = Heating - Energy_Loss
+            Per_min_Energy = Per_Second_change * 60 * time_res
 
-        Time = Time + time_res
-        Time_Data.append(Time)
-        Tempno = Tempno + 1;
-        Power_df = Power_df.append({'Power': Heating / 1000}, ignore_index=True)
+            Inside_Temp_Heating = (Per_min_Energy) / (Density_Air * Room_Volume) / SHC_Air
+            Inside_Temp = Inside_Temp + Inside_Temp_Heating
+
+            Time = Time + time_res
+            Time_Data.append(Time)
+            Tempno = Tempno + 1
+            Power_df = Power_df.append({'Power': Heating / 1000}, ignore_index=True)
         
     Power_df.index = MaskedOutsideTemp.index
     return Power_df
