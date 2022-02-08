4d1f6df28eee4925d71a7090a1bd64b12c7fb422
ajh2 <ajhonnywill@gmail.com>
2022-02-08 14:47:35 +0000

    Merged integrated heating

--- shems/IntergratedHeating.py
+++ shems/IntergratedHeating.py
@@ -90,14 +90,7 @@ def main(arrival_time, departure_time, time_resolution):
         else:
             Heating = Energy_Loss
 
-<<<<<<< HEAD
-        Wall_Loss = WallU * (Wall_Area * 4) * Outside_Temp_Change
-        Floor_Loss = FloorU * (Wall_Length * 2) * Outside_Temp_Change
-            Roof_Loss = RoofU * (Wall_Length * 2) * Outside_Temp_Change
-        Window_Loss = WindowU * (Window_Area * No_Windows) * Outside_Temp_Change
-        Door_Loss = DoorU * (Door_Area * No_Doors) * Outside_Temp_Change
-        Energy_Loss = (Wall_Loss + Floor_Loss + Roof_Loss + Window_Loss + Door_Loss) * 1.1
-=======
+
             Outside_Temp = MaskedOutsideTemp.iloc[Tempno, 0]
             Outside_Temp_Change = Inside_Temp - Outside_Temp
 
@@ -107,7 +100,6 @@ def main(arrival_time, departure_time, time_resolution):
             Window_Loss = WindowU * (Window_Area * No_Windows) * Outside_Temp_Change
             Door_Loss = DoorU * (Door_Area * No_Doors) * Outside_Temp_Change
             Energy_Loss = (Wall_Loss + Floor_Loss + Roof_Loss + Window_Loss + Door_Loss) * 1.1
->>>>>>> main
 
             Per_Second_change = Heating - Energy_Loss
             Per_min_Energy = Per_Second_change * 60 * time_res
