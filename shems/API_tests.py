17ab90cb55762e80ffbb14a9385826e5756cbad7
Yazzad Sukhia <yazzadsukhia@Yazads-MacBook-Pro.local>
2022-02-22 21:27:38 +0000

    API working and formed into working functions

--- shems/API_tests.py
+++ shems/API_tests.py
@@ -1,39 +1,67 @@
 """
-Contains inputs needed to work out charge demand requirement for an EV
+Contains
 
 Yazad Sukhia
 Feb 2022
 """
 
 
-# import googlemaps  # pip install googlemaps (or pycharm installation package)
-import json
 import requests
-import pandas as pd
 
-API_KEY = 'AIzaSyCsNLYOColvC8uLS7EeNMRi5nK1kr_KSp8'
-# map_client = googlemaps.Client(API_KEY)
+def main():
+       response = initialise_API_data()
+       print(response.text)
+       journey_start = journey_origin()
+       journey_end = journey_destination()
+       time_minutes = journey_time()
+       print("Journey from", journey_start, "to", journey_end, "will take", time_minutes, "minutes")
+       distace_km = journey_distance()
+       print("Journey from", journey_start, "to", journey_end, "will traverse", distace_km, "km")
+       return
 
-journey_start = "Bristol, UK, BS8 2AB"
-journey_end = "Bristol, UK, BS1 2NJ"
+def journey_origin():
+       journey_start = "Bristol, UK, BS8 2AB"
+       return journey_start
 
-# base url
-# url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
-# r = requests.get(url + "origins=" + journey_start + "&destinations=" + journey_end + "&key" + API_KEY)
+def journey_destination():
+       journey_end = "Bristol, UK, BS1 2NJ"
+       return journey_end
 
-url = ('https://maps.googleapis.com/maps/api/distancematrix/json'
-       + '?language=en-US&units=imperial'
-       + '&origins={}'
-       + '&destinations={}'
-       + '&key={}'
-       ).format(journey_start, journey_end, API_KEY)
+def initialise_API_data():
+       API_KEY = 'AIzaSyCsNLYOColvC8uLS7EeNMRi5nK1kr_KSp8'
+       journey_start = journey_origin()
+       journey_end = journey_destination()
+       url = ('https://maps.googleapis.com/maps/api/distancematrix/json'
+              + '?language=en-US&units=imperial'
+              + '&origins={}'
+              + '&destinations={}'
+              + '&key={}'
+              ).format(journey_start, journey_end, API_KEY)
+       payload = {}
+       headers = {}
+       response = requests.request("GET", url, headers=headers, data=payload)
+       # print(response.text)
+       return response
 
-payload={}
-headers = {}
+def journey_time():
+       response = initialise_API_data()
+       time_seconds = response.json()["rows"][0]["elements"][0]["duration"]["value"]
+       time_minutes = round(time_seconds / 60, 2)
+       return time_minutes
 
-response = requests.request("GET", url, headers=headers, data=payload)
+def journey_distance():
+       response = initialise_API_data()
+       distance_metres = response.json()["rows"][0]["elements"][0]["distance"]["value"]
+       distance_km = round(distance_metres / 1000, 2)
+       return distance_km
 
-print(response.text)
+# def journey_time_traffic():
+#        response = initialise_API_data()
+#
+
+if __name__ == "__main__":
+    main()
+
 # # get response
 # r = requests.get(url + "origins=" + journey_start + "&destinations=" + journey_end + "&key" + API_KEY)
 #
