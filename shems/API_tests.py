bab243aeda5d7d4dfcae86e1cd4b469d7ae02cfb
Yazzad Sukhia <yazzadsukhia@Yazads-MacBook-Pro.local>
2022-02-22 23:35:43 +0000

    inclusion of pessimistic journey time in traffic

--- shems/API_tests.py
+++ shems/API_tests.py
@@ -1,42 +1,51 @@
 """
-Contains
+Script contains function which enable links to the googlemaps API service, allowing the
+distance and tie of a journey to be calculated based on two addresses; defining a journey
+origin and final destination
+
+These two inputs should be changed in the separate input file and read into this script
 
 Yazad Sukhia
 Feb 2022
 """
 
-
 import requests
 
 def main():
-       response = initialise_API_data()
+       response = initialise_api_data()
        print(response.text)
        journey_start = journey_origin()
        journey_end = journey_destination()
        time_minutes = journey_time()
        print("Journey from", journey_start, "to", journey_end, "will take", time_minutes, "minutes")
+       ttime_minutes = journey_time_traffic()
+       print("Journey from", journey_start, "to", journey_end, "will take", ttime_minutes, "minutes in traffic")
        distace_km = journey_distance()
        print("Journey from", journey_start, "to", journey_end, "will traverse", distace_km, "km")
        return
 
 def journey_origin():
-       journey_start = "Bristol, UK, BS8 2AB"
+       journey_start = "Bristol, UK, BS8 2AB"  # This should be read from the input file
        return journey_start
 
 def journey_destination():
-       journey_end = "Bristol, UK, BS1 2NJ"
+       journey_end = "Bristol, UK, BS1 2NJ"  # This should be read from the input file
        return journey_end
 
-def initialise_API_data():
+def initialise_api_data():
        API_KEY = 'AIzaSyCsNLYOColvC8uLS7EeNMRi5nK1kr_KSp8'
        journey_start = journey_origin()
        journey_end = journey_destination()
+       departure_time = 'now'
+       traffic_model = 'pessimistic'
        url = ('https://maps.googleapis.com/maps/api/distancematrix/json'
               + '?language=en-US&units=imperial'
               + '&origins={}'
               + '&destinations={}'
               + '&key={}'
-              ).format(journey_start, journey_end, API_KEY)
+              + '&departure_time={}'
+              + '&traffic_model={}'
+              ).format(journey_start, journey_end, API_KEY, departure_time, traffic_model)
        payload = {}
        headers = {}
        response = requests.request("GET", url, headers=headers, data=payload)
@@ -44,37 +53,22 @@ def initialise_API_data():
        return response
 
 def journey_time():
-       response = initialise_API_data()
+       response = initialise_api_data()
        time_seconds = response.json()["rows"][0]["elements"][0]["duration"]["value"]
        time_minutes = round(time_seconds / 60, 2)
        return time_minutes
 
 def journey_distance():
-       response = initialise_API_data()
+       response = initialise_api_data()
        distance_metres = response.json()["rows"][0]["elements"][0]["distance"]["value"]
        distance_km = round(distance_metres / 1000, 2)
        return distance_km
 
-# def journey_time_traffic():
-#        response = initialise_API_data()
-#
+def journey_time_traffic():
+       response = initialise_api_data()
+       ttime_seconds = response.json()["rows"][0]["elements"][0]["duration_in_traffic"]["value"]
+       ttime_minutes = round((ttime_seconds / 60), 2)
+       return ttime_minutes
 
 if __name__ == "__main__":
     main()
-
-# # get response
-# r = requests.get(url + "origins=" + journey_start + "&destinations=" + journey_end + "&key" + API_KEY)
-#
-# # return time as text and seconds
-# time = r.json()["rows"][0]["elements"][0]["duration"]["text"]
-# seconds = r.json()["rows"][0]["elements"][0]["duration"]["value"]
-#
-# # print what we have so far
-# print("the total journey time is", time)
-
-# def define_journey():
-#     API_KEY = 'AIzaSyC08mMBGKbKzHBZ2FX7JmUsv3Kh9KPkIPg'
-#     map_client = googlemaps.Client(API_KEY)
-#     journey_start = "Bristol, UK, BS8 2AB" # This should be read from input file
-#     journey_end = "Bristol, UK, BS1 2NJ" # This should be read from input file
-#     return API_KEY, map_client, journey_start, journey_end
